e3e6fbfe65823e07fcae713d6eefd1ad95116964
Jay <jay@tezsure.com>
2021-06-07 13:45:26 +0530

    bugs fixed

--- lib/chain/tezos/tezos_node_writer.dart
+++ lib/chain/tezos/tezos_node_writer.dart
@@ -277,8 +277,6 @@ class TezosNodeWriter {
         'signature': signedOpGroup['signature']
       }
     ];
-    // print("signedOpGroup['signature'] ===> ${signedOpGroup['signature']}");
-    // print("parameters ===> ${jsonEncode(payload)}");
     var response = await HttpHelper.performPostRequest(server,
         'chains/$chainid/blocks/head/helpers/preapply/operations', payload);
     var json;
@@ -359,7 +357,6 @@ class TezosNodeWriter {
       int counter) {
     var parsedCode;
     var parsedStorage;
-    // print("using code Format ==> ${codeFormat.toString()}");
     if (codeFormat == TezosParameterFormat.Michelson) {
       parsedCode =
           jsonDecode(TezosLanguageUtil.translateMichelsonToMicheline(code));
